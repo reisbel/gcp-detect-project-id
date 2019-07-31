79a2a295eb873c750befad06da2df43382543fd3
Reisbel Machado <reisbel@gmail.com>
2019-07-31 12:32:22 -0400

    Add working sample

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace gcp_detect_project_id
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var projectId = GetProjectId();
+            Console.WriteLine($"Project-id: {projectId.Result}");
+            Console.ReadLine();
+        }
+
+        static async Task<string> GetProjectId()
+        {
+            try
+            {
+                //Metadata GCP URL
+                string baseUrl = "http://metadata.google.internal/computeMetadata/v1/project/project-id";
+               
+                //Create a new instance of HttpClient
+                using (HttpClient client = new HttpClient())
+                {
+                    //Add required Google Header to request this info
+                    client.DefaultRequestHeaders.Add("Metadata-Flavor","Google");
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            return await content.ReadAsStringAsync();
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+           
+            return string.Empty;
+   }   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # gcp-detect-project-id
+
 Detect project id running a .net core app inside a Google Compute Instance
+
+## Steps
+
+Clone repository inside a GCP instance
+
+```bash
+git clone https://github.com/reisbel/gcp-detect-project-id.git && cd gcp-detect-project-id
+```
+
+Restore the dependencies of the project.
+
+```bash
+dotnet restore
+```
+
+Run the project
+
+```bash
+dotnet run
+```
+
+```bash
+Project-id: test-project-id
+```
